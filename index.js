c07edf9dd7899efa60ac38fc0d81a22a6eb01ea6
Ayush <ayush.raj.sd@gmail.com>
2024-07-26 00:03:22 +0530

    lecture 2 express and middleware

--- index.js
+++ index.js
@@ -1,140 +1,72 @@
-// /**
-//  *
-//  * fs module - provides a way to work with the file system
-//  * fs module is a core module that comes with Node.js
-//  */
+const express = require("express");
 
-// const fs = require("fs");
+// create an express application
+const app = express();
+app.use(express.json());
 
-// // fs.readFile("file.txt", "utf8", (err, data) => {
-// //   // callback follows an error first callback pattern
-// //   if (err) {
-// //     console.log(err);
-// //     return;
-// //   } else {
-// //     console.log(data);
-// //   }
-// // });
+const loggerMiddleware = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next(); // call the next middleware or route handler
+};
 
-// // const content = "Hello World";
-// // fs.writeFile("example.txt", content, "utf8", (err) => {
-// //   if (err) {
-// //     console.log(err);
-// //     return;
-// //   }
-// //   console.log("File written successfully");
-// // });
+// app.use(loggerMiddleware);
 
-// // fs.rename("./example.txt", "new-example.txt", (err) => {
-// //   if (err) {
-// //     console.log(err);
-// //     return;
-// //   }
-// //   console.log("File renamed successfully");
-// // });
-
-// // fs.stat("my-dir", (err, stats) => {
-// //   if (err) {
-// //     console.log(err);
-// //     return;
-// //   } else {
-// //     console.log(stats.size);
-// //     console.log(stats.isDirectory());
-// //   }
-// // });
-
-// const directoryName = "my-directory";
-// // fs.mkdir(directoryName, (err) => {
-// //   if (err) {
-// //     console.log("Errior", err);
-// //     return;
-// //   }
-// //   console.log("Directory created successfully");
-// // });
-
-// // fs.rmdir(directoryName, { recursive: true }, (err) => {
-// //   if (err) {
-// //     console.log(err);
-// //     return;
-// //   }
-// //   console.log("Directory deleted successfully");
-// // });
-
-// /**
-//  * path module - provides utilities for working with file and directory paths across different operating systems
-//  * file paths differ from one operating system to another
-//  * windows uses backslashes (\) while unix ( POSIX ) based systems use forward slashes (/)
-//  */
-
-// // const path = require("path");
-
-// // const fullPath = path.join("folder", "subFolder", "file.txt");
-// // console.log(fullPath);
-
-// // const fileName = path.basename(fullPath);
-// // console.log("filename", fileName);
-
-// // const extName = path.extname(fullPath);
-// // console.log("extension", extName);
-
-// // const pathInfo = path.parse(fullPath);
-// // console.log(pathInfo);
-
-// // const normalizedPath = path.normalize("/path/to/../file.txt"); // relative path -> . ..
-// // console.log(normalizedPath);
-
-// // const complexPath = "/path/./to/../to/../../file.txt";
-// // const normalizedPath = path.normalize(complexPath);
-// // console.log("normalized", normalizedPath);
-
-// const sourceFilePath = "./dir/file.txt";
-// const destinationFilePath = "./destination-file.txt";
-
-// // create a readable stream
-// const readStream = fs.createReadStream(sourceFilePath);
-
-// // create a writable stream
-// const writeStream = fs.createWriteStream(destinationFilePath);
-
-// // pipe the read stream to the write stream
-// readStream.pipe(writeStream);
-
-// readStream.on("error", (err) => {
-//   console.error("Error reading the file", err);
-// });
-
-// writeStream.on("error", (err) => {
-//   console.error("Error writing the file", err);
-// });
-
-// writeStream.on("finish", () => {
-//   console.log("File written successfully");
-// });
-
-const http = require("http");
-const server = http.createServer((req, res) => {
-  // handle incoming requests
-  res.setHeader("Content-type", "application/json"); // mime - types
-  //   res.write("Hello World");
-  //   res.write("<html><head><title>Node Js Http Server</title></head><body>");
-  //   res.write("<h1>Hello World from Node</h1>");
-  //   res.write("</body></html>");
-  const jsonData = {
-    message: "Hello World",
-    date: new Date(),
-  };
-
-  const jsonResponse = JSON.stringify(jsonData);
-  res.write(jsonResponse);
-  res.end();
+// define a route
+app.get("/", (req, res) => {
+  res.send("Hello, Express");
 });
 
-const port = 8001;
-const host = "localhost";
-server.listen(port, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+app.get("/about", (req, res) => {
+  res.send(" This is the about page");
+});
+app.post("/data", (req, res) => {
+  console.log("req obj", req.body);
+  res.send("received a new POST request");
 });
 
-/**
- * ephemereal ports
- */
+const users = [
+  { id: 1, name: "User 1" },
+  { id: 2, name: "User 2" },
+];
+
+app.post("/users", (req, res) => {
+  const newUser = req.body; // {name:node}
+  const userId = users.length + 1; // 2 + 1 = 3 -> userId = 3
+  newUser.id = userId; // {name:node, id:3}
+  users.push(newUser);
+  res.status(201).json({ message: "User created", user: newUser });
+});
+
+// delete the user
+
+app.delete("/users/:id", (req, res) => {
+  // /users/1
+  const userId = req.params.id;
+  // find the user index matching the userId
+  const userIndex = users.findIndex((user) => user.id == userId);
+  if (userIndex == -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  // remove the user from the array
+  users.splice(userIndex, 1);
+  res.json({ message: "User deleted" });
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/special", loggerMiddleware, (req, res) => {
+  res.send("This is a special page");
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
